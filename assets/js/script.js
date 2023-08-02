1d73eccc6a5fcfa14f8b0187f5082377b3e923af
Volodymyr Popov <texnolife@gmail.com>
2023-08-02 22:29:34 +0100

    Display + question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,26 +6,40 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 });
 
 /**
  * Tha main game "loop", called when the script is first loaded
  * 
  */
-function runGame() {
-    let num1 = Math.floor(Math.random()*25) + 1;
-    let num2 = Math.floor(Math.random()*25) + 1;
- }
+function runGame(gameType) {
+
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown Game type: ${gameType}`);
+        throw `Unknown Game type: ${gameType}. Aborting!`;
+    }
+}
 
 
 function checkAnswer() { }
 function calculateCorrectAnswer() { }
 function incrementScore() { }
 function incrementWrongAnswer() { }
-function displayAdditionQuestion() { }
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('oparend1').textContent = operand1;
+    document.getElementById('oparend2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+}
 function subtractQuestion() { }
 function displayMultilyQuestion() { }
